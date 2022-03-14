0d0efa171055362b1119e684a1870d89785d48cc
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-14 19:35:56 +0100

    add list working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,20 @@ export class AppComponent {
     this.list = [];
     this.todoValue = "";
   }
+
+  addItem(){
+    if(this.todoValue !== ""){
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      }
+      this.list.push(newItem)
+    }
+    this.todoValue ="";
+  }
+
+  deleteItem(id:number){
+    this.list = this.list.filter(a => a.id !== id);
+  }
 }
